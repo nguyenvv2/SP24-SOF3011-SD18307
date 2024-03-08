9ce07d2acb602f2fc80f9c901ba348051ef88884
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2024-03-08 13:48:52 +0700

    update

--- src/main/java/com/example/sd18307/model/SinhVien.java
+++ src/main/java/com/example/sd18307/model/SinhVien.java
@@ -1,5 +1,14 @@
 package com.example.sd18307.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor// khong tham so
+@AllArgsConstructor // co tham so
 public class SinhVien {
 
     private String maSv;
@@ -10,45 +19,5 @@ public class SinhVien {
 
     private String diaChi;
 
-    public SinhVien() {
-    }
 
-    public SinhVien(String maSv, String tenSv, Integer tuoi, String diaChi) {
-        this.maSv = maSv;
-        this.tenSv = tenSv;
-        this.tuoi = tuoi;
-        this.diaChi = diaChi;
-    }
-
-    public String getMaSv() {
-        return maSv;
-    }
-
-    public void setMaSv(String maSv) {
-        this.maSv = maSv;
-    }
-
-    public String getTenSv() {
-        return tenSv;
-    }
-
-    public void setTenSv(String tenSv) {
-        this.tenSv = tenSv;
-    }
-
-    public Integer getTuoi() {
-        return tuoi;
-    }
-
-    public void setTuoi(Integer tuoi) {
-        this.tuoi = tuoi;
-    }
-
-    public String getDiaChi() {
-        return diaChi;
-    }
-
-    public void setDiaChi(String diaChi) {
-        this.diaChi = diaChi;
-    }
 }
