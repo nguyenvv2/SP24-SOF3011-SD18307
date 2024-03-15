c884669bce0f3e52e048597af1adf3569912c556
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2024-03-15 14:21:18 +0700

    update

--- src/main/java/com/example/sd18307/servlet/HomeServlet.java
+++ src/main/java/com/example/sd18307/servlet/HomeServlet.java
@@ -8,36 +8,83 @@ import jakarta.servlet.annotation.*;
 import java.io.IOException;
 import java.util.ArrayList;
 
-@WebServlet(name = "HomeServlet", value = {"/sinh-vien/trang-chu",
-        "/sinh-vien/add",
+@WebServlet(name = "HomeServlet", value = {"/sinh-vien/trang-chu", // GET
+        "/sinh-vien/add", // POST
+        "/sinh-vien/detail", //GET
+        "/sinh-vien/update/*",// POST
 })
 public class HomeServlet extends HttpServlet {
     ArrayList<SinhVien> list;
+    ArrayList<String> lops;
 
     public HomeServlet() {
         list = new ArrayList<>();
         SinhVien sinhVien = new SinhVien("Ph12345", "Nguyen Van A", 20, "HCM", "Nam", "");
         list.add(sinhVien);
         list.add(new SinhVien("Ph12311", "Nguyen Thi B", 21, "HN", "Nam", ""));
+
+        lops = new ArrayList<>();
+        lops.add("SD123");
+        lops.add("Sd124");
+        lops.add("SD125");
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("listSinhVien", list);
-        request.getRequestDispatcher("/sinh-vien/trang-chu.jsp").forward(request, response);
+        String uri = request.getRequestURI();
+        System.out.println(uri);
+        if (uri.contains("/sinh-vien/trang-chu")) {
+            request.setAttribute("lops", lops);
+            request.setAttribute("listSinhVien", list);
+            request.getRequestDispatcher("/sinh-vien/trang-chu.jsp").forward(request, response);
+        } else if (uri.contains("/sinh-vien/detail")) {
+            // mo trang detail
+            String id = request.getParameter("id");
+            SinhVien sinhVien = new SinhVien();
+            for (SinhVien sv : list) {
+                if (sv.getMaSv().equals(id)) {
+                    sinhVien = sv;
+                }
+            }
+            request.setAttribute("sinhVien", sinhVien);
+            request.setAttribute("lops", lops);
+            request.getRequestDispatcher("/sinh-vien/chi-tiet.jsp").forward(request, response);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String maSinhVien = request.getParameter("maSinhVien");
-        String tenSinhVien = request.getParameter("tenSinhVien");
-        Integer tuoi = Integer.parseInt(request.getParameter("tuoi"));
-        String diaChi = request.getParameter("diaChi");
-        String gioiTinh = request.getParameter("gioiTinh");
-        System.out.println(gioiTinh);
-        SinhVien sinhVien = new SinhVien(maSinhVien, tenSinhVien, tuoi, diaChi, gioiTinh, "");
-        list.add(sinhVien);
+        String uri = request.getRequestURI();
+        if (uri.contains("/sinh-vien/add")) {
+            String maSinhVien = request.getParameter("maSinhVien");
+            String tenSinhVien = request.getParameter("tenSinhVien");
+            Integer tuoi = Integer.parseInt(request.getParameter("tuoi"));
+            String diaChi = request.getParameter("diaChi");
+            String gioiTinh = request.getParameter("gioiTinh");
+            String lop = request.getParameter("lop");
+            SinhVien sinhVien = new SinhVien(maSinhVien, tenSinhVien, tuoi, diaChi, gioiTinh, lop);
+            list.add(sinhVien);
 
-        response.sendRedirect("/sinh-vien/trang-chu");
+            response.sendRedirect("/sinh-vien/trang-chu");
+        } else if (uri.contains("/sinh-vien/update")) {
+            // lam update
+            String maSinhVien = request.getPathInfo();
+            String tenSinhVien = request.getParameter("tenSinhVien");
+            System.out.println("Ma sinh vien :" + maSinhVien);
+//            Integer tuoi = Integer.parseInt(request.getParameter("tuoi"));
+//            String diaChi = request.getParameter("diaChi");
+//            String gioiTinh = request.getParameter("gioiTinh");
+//            String lop = request.getParameter("lop");
+//            for (SinhVien sv : list) {
+//                if (maSinhVien.equals(sv.getMaSv())) {
+//                    sv.setTuoi(tuoi);
+//                    sv.setGioiTinh(gioiTinh);
+//                    sv.setDiaChi(diaChi);
+//                    sv.setTenSv(tenSinhVien);
+//                    sv.setTenLop(lop);
+//                }
+//            }
+//            response.sendRedirect("/sinh-vien/trang-chu");
+        }
     }
 }
--- src/main/webapp/sinh-vien/chi-tiet.jsp
+++ src/main/webapp/sinh-vien/chi-tiet.jsp
@@ -1,36 +1,38 @@
 <%--
   Created by IntelliJ IDEA.
   User: nguyenvv
-  Date: 08/03/2024
-  Time: 12:55
+  Date: 15/03/2024
+  Time: 12:28
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib uri="http://java.sun.com/jstl/core_rt" prefix="c" %>  <%--import thư viện vào jsp --%>
+
 <html>
 <head>
-    <title>Sinh vien</title>
+    <title>Title</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 <body class="container">
 
-<form action="/sinh-vien/add" method="post">
+<form action="/sinh-vien/update/${sinhVien.maSv}" method="post">
     <div class="mb-3">
         <label class="form-label">Ma sinh vien</label>
-        <input type="text" class="form-control" name="maSinhVien">
+        <input type="text" class="form-control" name="maSinhVien" value="${sinhVien.maSv}">
     </div>
     <div class="mb-3">
         <label class="form-label">Ten sinh vien</label>
-        <input type="text" class="form-control" name="tenSinhVien">
+        <input type="text" class="form-control" name="tenSinhVien" value="${sinhVien.tenSv}">
     </div>
     <div class="mb-3">
         <label class="form-label">Tuoi</label>
-        <input type="text" class="form-control" name="tuoi">
+        <input type="text" class="form-control" name="tuoi" value="${sinhVien.tuoi}">
     </div>
     <div class="mb-3">
         <label class="form-label">Dia Chi</label>
-        <input type="text" class="form-control" name="diaChi">
+        <input type="text" class="form-control" name="diaChi" value="${sinhVien.diaChi}">
     </div>
     <div class="mb-3">
         <label for="disabledSelect" class="form-label">Ten lop</label>
@@ -56,38 +58,7 @@
             </label>
         </div>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary">Cap nhat</button>
 </form>
-<h1>Danh sach sinh vien</h1>
-<table class="table">
-    <thead>
-    <tr>
-        <td>STT</td>
-        <td>Ma Sinh Vien</td>
-        <td>Ho ten</td>
-        <td>Dia chi</td>
-        <td>Tuoi</td>
-        <td>Gioi tinh</td>
-        <td>Lop</td>
-        <td>Chuc nang</td>
-    </tr>
-    </thead>
-    <tbody>
-    <c:forEach var="s" items="${listSinhVien}" varStatus="i">
-        <tr>
-            <td>${i.index + 1}</td>
-            <td>${s.maSv}</td>
-            <td>${s.tenSv}</td>
-            <td>${s.diaChi}</td>
-            <td>${s.tuoi}</td>
-            <td>${s.gioiTinh}</td>
-            <td>${s.tenLop}</td>
-            <td>
-                <a href="/sinh-vien/detail?id=${s.maSv}" class="btn btn-primary">Chi tiet</a>
-            </td>
-        </tr>
-    </c:forEach>
-    </tbody>
-</table>
 </body>
 </html>
