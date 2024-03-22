c888b5bcb81717a704333d335eb3337485e3fd3b
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2024-03-22 12:31:01 +0700

    update

---
+++ src/main/java/com/example/sd18307/connect/HibernateUtils.java
@@ -0,0 +1,40 @@
+package com.example.sd18307.connect;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
+public class HibernateUtils {
+    private static final SessionFactory FACTORY;
+
+    static {
+        Configuration conf = new Configuration();
+
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
+        properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=java4");
+        properties.put(Environment.USER, "sa");
+        properties.put(Environment.PASS, "Password.1");
+        properties.put(Environment.SHOW_SQL, "true");
+
+        conf.setProperties(properties);
+//        conf.addAnnotatedClass(DongVat.class);
+        ServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .applySettings(conf.getProperties()).build();
+        FACTORY = conf.buildSessionFactory(registry);
+
+    }
+
+    public static SessionFactory getFACTORY() {
+        return FACTORY;
+    }
+
+    public static void main(String[] args) {
+        getFACTORY();
+    }
+}
